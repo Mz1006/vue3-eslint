704cb1145e1b30ef6ca85cfb1ed1b79a3efffb20
zhangmengzhu <zmz.bajiezu.cn>
2021-06-15 14:10:24 +0800

    添加验证

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,  // 禁止套娃
+  parserOptions: {
+    parser: 'babel-eslint',  // babel解析
+    sourceType: 'module',
+  },
+	parser: "vue-eslint-parser",
+  env: { // 环境变量
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  // 扩展风格
+  extends: ['airbnb','plugin:vue/recommended', 'eslint:recommended'],
+  rules: {
+    // 规则
+    "quotes": [1, "single"],            // 单引号
+    "quote-props":[2, "as-needed"],     // 双引号自动变单引号
+    "semi": [2, "never"],               // 一行结尾不要写分号
+    "comma-dangle": [1,"always-multiline"]  // 对象或数组多行写法时，最后一个值加逗号
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
@@ -9,8 +14,8 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 }
 </script>
 
